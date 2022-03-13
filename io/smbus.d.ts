abfe6efcb77f90bc485ffb3dd5a34dbd0bc5f9e8
MKGaru <dev@igaru.com>
2022-03-13 18:18:46 +0900

    add io/smbus

---
+++ io/smbus.d.ts
@@ -0,0 +1,38 @@
+declare module "embedded:io/smbus" {
+	import { I2COptions } from "embedded:io/i2c";
+
+	export interface SMBusOptions extends I2COptions {
+		sendStop?: boolean;
+	}
+
+	class SMBus {
+	
+		constructor(options: SMBusOptions);
+		close(): void;
+	
+		readByte(register: number): number;
+	
+		readWord(register: number, bigEndian: boolean): number;
+	
+		readBlock(register: number, buffer: boolean): number;
+	
+		writeByte(register: number, byte: number): void;
+	
+		writeWord(register: number, word: number, bigEndian: boolean): void;
+	
+		writeBlock(register: number, buffer: ArrayBuffer|Uint8Array): void;
+	
+		sendByte(command: number): void;
+	
+		receiveByte(): number;
+	
+		readQuick(): void;
+	
+		writeQuick(): void;
+	
+		get format(): "buffer";
+		set format(value: "buffer");
+	}
+	
+	export default SMBus;
+}
